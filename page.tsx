17e79ac0f2306e4537457f78d4dccb023a08f711
aliulmurtaza051 <157344647+aliulmurtaza051@users.noreply.github.com>
2024-01-23 01:48:23 -0800

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='text-5xl text-black-500 font-bold text-center' >ali ul murtaza</div>
+  )
+}
+
+export default page
